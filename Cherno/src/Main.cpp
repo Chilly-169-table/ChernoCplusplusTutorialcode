b4c0895a0aaa0cd2259dc81e5a75d6dc7aa5f0d6
Hemanth Lvssr <hemanthlvssr66@gmail.com>
2021-02-01 18:49:28 +0530

    Add project files.

---
+++ Cherno/src/Main.cpp
@@ -0,0 +1,7 @@
+#include <iostream>  //Pre-Processor statement //Processed right before compilation,hence the name //So all contents of iostream are copied here
+
+int main()
+{
+	std::cout << "Hello World" << std::endl;
+	std::cin.get(); //waits until user presses enter to start executing new line.
+}
